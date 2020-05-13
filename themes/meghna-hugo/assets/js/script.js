9092411e3bb2f8302c3a6f59b8957f3dadb13475
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-05-13 16:28:29 +0200

    working menu

--- themes/meghna-hugo/assets/js/script.js
+++ themes/meghna-hugo/assets/js/script.js
@@ -1,27 +1,26 @@
-/* ========================================================================= */
-/*	Page Preloader
-/* ========================================================================= */
+'use strict';
 
 jQuery(function ($) {
-	"use strict";
+	/* ================================================== */
+	/*	Lazy load initialize
+	/* ================================================== */
 
-	/* ========================================================================= */
-	/*	lazy load initialize
-	/* ========================================================================= */
-
-	const observer = lozad(); // lazy loads elements with default selector as ".lozad"
+    // Lazy loads elements with default selector as ".lozad"
+	const observer = lozad();
 	observer.observe();
 
-	/* ========================================================================= */
+	/* ================================================== */
 	/*	Magnific popup
-	/* =========================================================================  */
+	/* ================================================== */
+
 	$('.image-popup').magnificPopup({
 		type: 'image',
 		removalDelay: 160, //delay removal by X to allow out-animation
 		callbacks: {
 			beforeOpen: function () {
 				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+				this.st.image.markup = this.st.image.markup.replace(
+                    'mfp-figure', 'mfp-figure mfp-with-anim');
 				this.st.mainClass = this.st.el.attr('data-effect');
 			}
 		},
@@ -31,34 +30,81 @@ jQuery(function ($) {
 		fixedBgPos: true
 	});
 
-	/* ========================================================================= */
+	/* ================================================== */
 	/*	Portfolio Filtering Hook
-	/* =========================================================================  */
+	/* ================================================== */
 
-	var containerEl = document.querySelector('.shuffle-wrapper');
+    // Maximum of three nested filters for the gallery.
+	const containerEl = document.querySelector('.shuffle-wrapper');
 	if (containerEl) {
-		var Shuffle = window.Shuffle;
-		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+		let Shuffle = window.Shuffle;
+		let myShuffle = new Shuffle(containerEl, {
 			itemSelector: '.shuffle-item',
 			buffer: 1
 		});
 
-		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-			var input = evt.currentTarget;
-			if (input.checked) {
-				myShuffle.filter(input.value);
-			}
+        // The main bar will never be selected, since it's always visible.
+		const bars = document.querySelectorAll(
+            '.portfolio-filter:not(.menu-level-one)');
+
+        // Checking if a single bar contains an input element with some value.
+        function contains(bar, value) {
+            for (let i = 0; i < bar.children.length; i++) {
+                if (bar.children[i].children[0].value === value) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // The hidden levels will appear and disappear depending on the first
+        // level.
+		$('input[name="shuffle-filter"]').on('change', evt => {
+            // The pressed button, and the bar it's in.
+			const input = evt.currentTarget;
+            const input_bar = input.parentNode.parentNode;
+			if (!input.checked) {
+                return
+            }
+
+            // Filtering the items with the new selected button.
+            myShuffle.filter(input.value);
+
+            // Updating the displayed rows.
+            // TODO HIDE ANIMATION AND RESET ACTIVE TO "Todo"
+            bars.forEach((bar) => {
+                console.log(bar, input_bar, input);
+                // Every bar will be hidden except for itself, its first
+                // child bar and its parents.
+                const bar_parent = bar.getAttribute('parent');
+                const input_bar_parent = input_bar.getAttribute('parent');
+                if (bar === input_bar  // Itself
+                        || contains(bar, input_bar_parent)
+                        || bar_parent === input.value) {  // Children
+                    if (bar.classList.contains('d-none')) {
+                        bar.classList.remove('d-none');
+                        bar.classList.add('d-flex');
+                    } else {
+
+                    }
+                } else {
+                    bar.classList.add('d-none');
+                    bar.classList.remove('d-flex');
+                }
+            });
 		});
 	}
 
-	/* ========================================================================= */
-	/*	animation scroll js
-	/* ========================================================================= */
+	/* ================================================== */
+	/*	Animation scroll js
+	/* ================================================== */
 
-	var html_body = $('html, body');
-	$('nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+	let html_body = $('html, body');
+    // use page-scroll class in any HTML tag for scrolling
+	$('nav a, .page-scroll').on('click', function () {
 		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-			var target = $(this.hash);
+			let target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
 				html_body.animate({
@@ -85,17 +131,18 @@ jQuery(function ($) {
 		}
 	});
 
-	/* ========================================================================= */
-	/*	counter up
-	/* ========================================================================= */
+	/* ================================================== */
+	/*	Counter up
+	/* ================================================== */
+
 	function counter() {
-		var oTop;
+		let oTop;
 		if ($('.count').length !== 0) {
 			oTop = $('.count').offset().top - window.innerHeight;
 		}
 		if ($(window).scrollTop() > oTop) {
 			$('.count').each(function () {
-				var $this = $(this),
+				let $this = $(this),
 					countTo = $this.attr('data-count');
 				$({
 					countNum: $this.text()
@@ -117,5 +164,4 @@ jQuery(function ($) {
 	$(window).on('scroll', function () {
 		counter();
 	});
-
 });
